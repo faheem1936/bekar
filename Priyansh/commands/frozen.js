d29aedc955ee8c12bc1604b685631dd96142b8a4
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 00:55:36 +0500

    Update frozen.js

--- Priyansh/commands/frozen.js
+++ Priyansh/commands/frozen.js
@@ -1,4 +1,4 @@
-const API = "https://5xp7m4-8080.csb.app/api/textpro?number=10&text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/textpro?number=10&text=";
 module.exports.config = {
   name: "frozen",
   version: "1.0.0",
