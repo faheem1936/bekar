0e86d89b1fa6d2814cf87f59aedd6136edcfabe4
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 01:02:06 +0500

    Update pubgcover.js

--- Priyansh/commands/pubgcover.js
+++ Priyansh/commands/pubgcover.js
@@ -1,4 +1,4 @@
-const API = "https://cjhvv2-3000.csb.app/api/ephoto/coverpubg?text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/ephoto/coverpubg?text=";
 module.exports.config = {
   name: "pubgcover",
   version: "1.0.0",
