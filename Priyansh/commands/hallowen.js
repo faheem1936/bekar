dd80d22147786cc539ad1d3fc733fff7cf841b5e
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 00:59:47 +0500

    Update hallowen.js

--- Priyansh/commands/hallowen.js
+++ Priyansh/commands/hallowen.js
@@ -1,4 +1,4 @@
-const API = "https://cjhvv2-3000.csb.app/api/ephoto/hallowen?text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/ephoto/hallowen?text=";
 module.exports.config = {
   name: "hallowen",
   version: "1.0.0",
