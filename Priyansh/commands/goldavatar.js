d292032e34a033418fedb65692c0be9127c4621e
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 00:57:42 +0500

    Update goldavatar.js

--- Priyansh/commands/goldavatar.js
+++ Priyansh/commands/goldavatar.js
@@ -1,4 +1,4 @@
-const API = "https://cjhvv2-3000.csb.app/api/ephoto/goldavatar?text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/ephoto/goldavatar?text=";
 module.exports.config = {
   name: "goldavatar",
   version: "30.0.10",
