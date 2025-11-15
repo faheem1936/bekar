db71e5b0c9ad484b6492a95d27abd4377cb4e291
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 01:02:24 +0500

    Update pubgvideo.js

--- Priyansh/commands/pubgvideo.js
+++ Priyansh/commands/pubgvideo.js
@@ -1,4 +1,4 @@
-const API = "https://cjhvv2-3000.csb.app/api/ephoto/pubgavatar?text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/ephoto/pubgavatar?text=";
 module.exports.config = {
   name: "pubgavatarvideo",
   version: "1.0.0",
