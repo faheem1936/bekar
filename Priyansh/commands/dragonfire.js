ed2a20e79364d03f81f6ffef6ccfe0928a454b02
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 00:50:31 +0500

    Update dragonfire.js

--- Priyansh/commands/dragonfire.js
+++ Priyansh/commands/dragonfire.js
@@ -1,4 +1,4 @@
-const API = "https://cjhvv2-3000.csb.app/api/ephoto/dragonfire?text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/ephoto/dragonfire?text=";
 module.exports.config = {
   name: "dragonfire",
   version: "1.0.0",
