f57c6863ea3839e949609f528101c0aa29f35c6a
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 00:59:02 +0500

    Update greenfl.js

--- Priyansh/commands/greenfl.js
+++ Priyansh/commands/greenfl.js
@@ -1,4 +1,4 @@
-const API = "https://cjhvv2-3000.csb.app/api/ephoto/greenbrush?text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/ephoto/greenbrush?text=";
 module.exports.config = {
   name: "greenfl",
   version: "1.0.0",
