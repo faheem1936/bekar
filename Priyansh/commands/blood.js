289abd2d2ca30598d1f668b6bfe242760f04ad3d
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 00:43:32 +0500

    Update blood.js

--- Priyansh/commands/blood.js
+++ Priyansh/commands/blood.js
@@ -1,4 +1,4 @@
-const API = "https://cjhvv2-3000.csb.app/api/ephoto/horror?text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/ephoto/horror?text=";
 module.exports.config = {
   name: "blood",
   version: "1.0.0",
