60d39be0fb4d8dc614f0792b2b174bf9b40978da
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2025-11-16 00:54:35 +0500

    Update fpsgame.js

--- Priyansh/commands/fpsgame.js
+++ Priyansh/commands/fpsgame.js
@@ -1,4 +1,4 @@
-const API = "https://cjhvv2-3000.csb.app/api/ephoto/fpsgame?text=";
+const API = "https://new--faheem-logo-api--89q4vtp4hz4m.code.run/api/ephoto/fpsgame?text=";
 module.exports.config = {
   name: "fpsgame",
   version: "1.0.0",
